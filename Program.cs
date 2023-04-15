85af83c6fc60df7e11c30f16690f8e5e02c6b1bb
xxWolfDogxx <matalasov.serzh@yandex.ru>
2023-04-15 22:57:18 +0500

    Authorization, registration, minimal basic functionality, as well as an installer and an uninstaller have been done

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ASI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Forms.Main.ASI());
+            Application.Run(new Auth());
         }
     }
 }
