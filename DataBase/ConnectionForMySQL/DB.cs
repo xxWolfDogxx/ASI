c7df00992c8d35eabce7b13710961ac69a2d56e0
Sergey <Sergey@DESKTOP-6V784NV>
2023-05-10 16:11:18 +0500

    готово

--- DataBase/ConnectionForMySQL/DB.cs
+++ DataBase/ConnectionForMySQL/DB.cs
@@ -4,13 +4,17 @@ namespace ASI.DataBase.ConnectionForMySQL
 {
     class DB
     {
+
         //MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root; password=root; database=asi");
         MySqlConnection connection = new MySqlConnection("server=100.100.44.70; port=3306; uid=root; pwd=hvost; database=asi");
 
         public void openConnection()
         {
-            if (connection.State == System.Data.ConnectionState.Closed)
-                connection.Open();
+
+                if (connection.State == System.Data.ConnectionState.Closed)
+                    connection.Open();               
+            
+            
         }
 
         public void closeConnection()
@@ -23,5 +27,8 @@ namespace ASI.DataBase.ConnectionForMySQL
         {
             return connection;
         }
+
+
+
     }
 }
